e236a3c16b382b80fa6fd9823c9bc80a9450a915
nandinirajput <109655286+nandinirajput@users.noreply.github.com>
2022-07-22 15:50:43 +0530

    Add files via upload

---
+++ variable_rules.py
@@ -0,0 +1,40 @@
+from calendar import c
+from signal import default_int_handler
+
+
+a=6
+number=8
+_=9
+print(_)
+print(a)
+print(number)
+# print(h)
+
+# _com5-7
+rt-3
+6t-7      inavalid SyntaxError
+c_r+7     name 'c_r' is not default
+khfselgyfdhfbufjbdskufsgkbd-6
+
+A=6;B=9
+print(A)
+print(B)
+C-A
+A-B
+B-c
+print(A)
+print(B)
+print(C)
+
+x=6;y=8
+print(x)
+x,y-y,x
+print(x)
+
+print('abc'.isidentifier())
+print('a b c'.isidentifier())
+print('_'.isidentifier())
+print('a_b_'.isidentifier())
+print('a3b_'.isidentifier())
+print('a3b_7'.isidentifier())
+print('43b_7'.isidentifier())
